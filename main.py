db967c0c9b5b2ccc3800603e80cf3edb84f39020
add365 <64209073+add365@users.noreply.github.com>
2024-06-02 13:57:03 +0800

    Update 新增db_setting

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import pymysql
 
 def connect_sql_server():
+    db_setting = {
+        "host": "127.0.0.1",
+        "user": "",
+        "password": "",
+        "database": "",
+        "charset": "utf8"
+    }
+    conn = pymysql.connect(**db_setting)
     print('SQL login')
+    return conn
 
 def main():
     print('Hello, World!')
