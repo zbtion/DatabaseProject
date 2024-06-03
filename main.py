3e90679685e1ee803c8334a1ad99446021674377
add365 <64209073+add365@users.noreply.github.com>
2024-06-03 16:59:40 +0800

    Update 更改import為pymssql

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-import pymysql
+import pymssql
+
 
 def connect_sql_server():
-    db_setting = {
+    db_settings = {
         "host": "127.0.0.1",
-        "user": "",
-        "password": "",
-        "database": "",
-        "charset": "utf8"
+        'user': 'add365',
+        'password': '1234',
+        'database': 'ncu_database',
+        'charset': 'utf8'
     }
-    conn = pymysql.connect(**db_setting)
+    conn = pymssql.connect(**db_settings)
     print('SQL login')
     return conn
 
+
 def main():
+    try:
+        conn = connect_sql_server()
+    except Exception as e:
+        print(e)
     print('Hello, World!')
+
 
 def print_result():
     print('Result')
+
 
 if __name__ == '__main__':
     main()
